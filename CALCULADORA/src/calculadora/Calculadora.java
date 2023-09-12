c72d8f31598b66ccdea145338c5dc500be5c921e
SUPORTE02 <SUPORTE02@DESKTOP-C2F5LV1>
2023-09-12 13:15:11 -0300

    Teste

---
+++ CALCULADORA/src/calculadora/Calculadora.java
@@ -0,0 +1,22 @@
+package calculadora;
+//classe calculadora para desenvolver o funcionamento dos calculos matematicos que serão utilizados no classe "calculadora_teste".
+public class Calculadora {
+	
+	//calculo de somar
+	public int somar(int a, int b) {
+		return a + b;
+	}
+	//calculo da subtração.
+	public int subt(int a, int b) {
+		return a - b;
+	}
+	//calculo da multriplicação.
+	public float multi(float a, float b) {
+		return a * b;
+	}
+	//calculo da divisão.
+	public float divi(float a, float b) {
+		return a / b;
+	}
+	
+}
---
+++ CALCULADORA/src/calculadora/calculadora_teste.java
@@ -0,0 +1,12 @@
+package calculadora;
+
+public class calculadora_teste {
+
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		int soma = calc.somar(3,7);
+		System.out.println(soma);
+
+	}
+
+}
